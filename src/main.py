5d0d5dd795dc3fa8e60d0992ebe3a3d241c02ebf
ITPhongou23 <nt.phong11405@gmail.com>
2026-04-21 14:28:17 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ load_dotenv(find_dotenv())
 hf_token = os.getenv("HF_TOKEN")
 
 pages = st.navigation([
-    st.Page("src/pages/Home.py", title="Trang chủ"),
-    st.Page("src/pages/FAQ.py", title="Hướng dẫn sử dụng"),
-    st.Page("src/pages/detector.py", title="Kiểm tra AI"),
+    st.Page("pages/Home.py", title="Trang chủ"),
+    st.Page("pages/FAQ.py", title="Hướng dẫn sử dụng"),
+    st.Page("pages/detector.py", title="Kiểm tra AI"),
 ])
 
 pages.run()
