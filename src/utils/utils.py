0b4666bb7152a46a3e71389f0735eb468539d415
ITPhongou23 <nt.phong11405@gmail.com>
2026-04-21 14:36:10 +0700

    Update utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -17,7 +17,7 @@ def init_page(title):
     )
 
     try:
-        with open("assets/css/styles.css") as f:
+        with open("/src/assets/css/styles.css") as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     except FileNotFoundError:
         pass
