fa98867f11c38e0c1c4d5071d054a48fd6e67cd2
ITPhongou23 <nt.phong11405@gmail.com>
2026-05-03 22:06:50 +0700

    Update file

--- src/pages/detector.py
+++ src/pages/detector.py
@@ -21,11 +21,9 @@ text_kq = ""
 
 
 #Hàm.
-@st.cache_resource
 def load_model(model_name):
     return pipeline("text-classification", model=model_name)
 
-@st.cache_resource
 def load_file_model(model_name):
     path = hf_hub_download(repo_id=model_name, filename="xgb_robust_final.joblib")
     return joblib.load(path)
